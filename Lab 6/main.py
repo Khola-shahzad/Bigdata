0ae25efcdb94d19a78582b6a5b025e0b46a249c8
Khola-shahzad <kholashahzad963@gmail.com>
2025-10-15 16:58:29 +0500

    Added Lab 6 Parquet task

---
+++ Lab 6/main.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import pyarrow as pa
+import pyarrow.parquet as pq
+
+data = [
+    {"id": 1, "name": "Khola", "age": 23},
+    {"id": 2, "name": "Ayesha", "age": 25},
+    {"id": 3, "name": "Hassan", "age": 28}
+]
+df = pd.DataFrame(data)
+df.to_parquet("sample_data.parquet", engine="pyarrow", index=False)
+
+print("Parquet file created successfully: sample_data.parquet")
+print(df)
